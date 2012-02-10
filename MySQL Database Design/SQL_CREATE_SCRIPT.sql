bdfbc500debca9b191501d2014dc7e42d9288ae9
thomhurks@gmail.com <thomhurks@gmail.com@91db6d75-a6cd-0381-b0a8-a56dc904e31a>
2012-02-10 02:13:41 +0000

    Expanded database with customer, employee, order and order_entry tables.

--- MySQL Database Design/SQL_CREATE_SCRIPT.sql
+++ MySQL Database Design/SQL_CREATE_SCRIPT.sql
@@ -61,6 +61,89 @@ CREATE  TABLE IF NOT EXISTS `hurksbestelsysteem`.`product_category` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `hurksbestelsysteem`.`employee`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `hurksbestelsysteem`.`employee` ;
+
+CREATE  TABLE IF NOT EXISTS `hurksbestelsysteem`.`employee` (
+  `idemployee` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`idemployee`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `hurksbestelsysteem`.`customer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `hurksbestelsysteem`.`customer` ;
+
+CREATE  TABLE IF NOT EXISTS `hurksbestelsysteem`.`customer` (
+  `idcustomer` INT NOT NULL AUTO_INCREMENT ,
+  `firstname` VARCHAR(45) NULL ,
+  `lastname` VARCHAR(45) NOT NULL ,
+  `phonenumber` VARCHAR(45) NULL ,
+  `street` VARCHAR(45) NULL ,
+  `streetnumber` VARCHAR(45) NULL ,
+  `town` VARCHAR(45) NULL ,
+  PRIMARY KEY (`idcustomer`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `hurksbestelsysteem`.`order`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `hurksbestelsysteem`.`order` ;
+
+CREATE  TABLE IF NOT EXISTS `hurksbestelsysteem`.`order` (
+  `idorder` INT NOT NULL AUTO_INCREMENT ,
+  `customerid` INT NOT NULL ,
+  `ordered_datetime` DATETIME NOT NULL ,
+  `pickup_datetime` DATETIME NOT NULL ,
+  `employee_id` INT NOT NULL ,
+  `description` VARCHAR(45) NULL ,
+  PRIMARY KEY (`idorder`) ,
+  INDEX `employee_id` (`employee_id` ASC) ,
+  INDEX `customerid` (`customerid` ASC) ,
+  CONSTRAINT `employee_id`
+    FOREIGN KEY (`employee_id` )
+    REFERENCES `hurksbestelsysteem`.`employee` (`idemployee` )
+    ON DELETE RESTRICT
+    ON UPDATE NO ACTION,
+  CONSTRAINT `customerid`
+    FOREIGN KEY (`customerid` )
+    REFERENCES `hurksbestelsysteem`.`customer` (`idcustomer` )
+    ON DELETE RESTRICT
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `hurksbestelsysteem`.`order_entry`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `hurksbestelsysteem`.`order_entry` ;
+
+CREATE  TABLE IF NOT EXISTS `hurksbestelsysteem`.`order_entry` (
+  `idorder_entry` INT NOT NULL AUTO_INCREMENT ,
+  `orderid` INT NOT NULL ,
+  `productid` INT NOT NULL ,
+  `quantity` INT NOT NULL ,
+  PRIMARY KEY (`idorder_entry`) ,
+  INDEX `orderid` (`orderid` ASC) ,
+  INDEX `productid` (`productid` ASC) ,
+  CONSTRAINT `orderid`
+    FOREIGN KEY (`orderid` )
+    REFERENCES `hurksbestelsysteem`.`order` (`idorder` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `productid`
+    FOREIGN KEY (`productid` )
+    REFERENCES `hurksbestelsysteem`.`product` (`idproduct` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
